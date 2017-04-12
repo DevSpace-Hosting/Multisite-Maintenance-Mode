4e3a01f8ab9dabfbb2b95b60761a074cfa1f69e0
Marc Woodyard <marc.woodyard@fleep.io>
2017-04-11 18:47:06 -0700

    Cleaned up code.

--- maintenance.php
+++ maintenance.php
@@ -9,37 +9,11 @@ Author URI: https://marcwoodyard.com
 */
 
 add_action('muplugins_loaded', 'show_maintenance_page', 999, 0);
-	function show_maintenance_page()
-	{
+	function show_maintenance_page() {
 		//If the user is not logged in and not on a login page.
-		if (!is_user_logged_in())
-		{
-			//Tell them the network is under maintence.
+		if (!is_user_logged_in() && !current_user_can('update_core')) {
 			wp_redirect( content_url('/maintenance.html/'));
-			exit(); 
-			//Then 
+			exit();
 		}
-		else
-		{
-			if((current_user_can( 'update_core'))
-			{
-				//Do nothing because this is a super admin working on the network.
-			}
-			else
-			{
-				//This isn't a super admin, so redirect them to the maintenance page.
-				wp_redirect( content_url('/maintenance.html/'));
-
-				/*
-				We could store the maintenance page in the plugin files, but I prefer to 
-				keep it with my other error page files.
-
-				wp_redirect( plugins_url('/maintenance.html/'));
-				*/
-		
-				// always call `exit()` after `wp_redirect`
-	            exit(); 
-			}
-		}
-	}
+}
 ?>
